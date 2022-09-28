14ab742bd6bfa41408c9d9507b0df2d0ab874f60
xZ3NER <peng.asiacita@gmail.com>
2022-09-28 19:16:24 +0100

    Añadido comentarios Actividad 1

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -11,12 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Crea el listener y le asigna su acción para el botón 'buttonReverse'
         reverseButtonListener()
 
+        //Crea el listener y le asigna su acción para el botón 'buttonSubString'
         extractButtonListener()
     }
 
     private fun extractButtonListener() {
+        //Carga el componente correspondiente al id en una constante
+        //findViewByID<'Clase del componente'>(R.id.'id del componente')
         val extractButton = findViewById<Button>(R.id.buttonSubString)
         val userInputEditText = findViewById<EditText>(R.id.editTextUserInput)
         val initialPosition = findViewById<EditText>(R.id.editTextInitialPos)
@@ -24,7 +28,9 @@ class MainActivity : AppCompatActivity() {
 
         val extractedText = findViewById<TextView>(R.id.textViewSubString)
 
+        //Listener que se activará al darle click
         extractButton.setOnClickListener {
+            //.text será equivalente a .getText() en java si se usa para almacenarse (var/val)
             val userInput = userInputEditText.text.toString()
             var initialPos = 0
             var finalPos = 0
@@ -37,6 +43,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (initialPos < finalPos && finalPos <= userInput.length && initialPos >= 0) {
+                //.text será equivalente a un .setText() en java si se usa igualando a algún valor
                 extractedText.text = userInput.substring(initialPos, finalPos)
             }
 
@@ -44,12 +51,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun reverseButtonListener() {
+        //Carga el componente correspondiente al id en una constante
+        //findViewByID<'Clase del componente'>(R.id.'id del componente')
         val reverseButton = findViewById<Button>(R.id.buttonReverse)
         val userInputEditText = findViewById<EditText>(R.id.editTextUserInput)
         val reversedText = findViewById<TextView>(R.id.textViewReversed)
 
+        //Listener que se activará al darle click
         reverseButton.setOnClickListener {
+            //.text será equivalente a .getText() en java si se usa para almacenarse (var/val)
             val userInput = userInputEditText.text.toString()
+
+            //.text será equivalente a un .setText() en java si se usa igualando a algún valor
+            //el método .reversed() devolverá una String inversa
             reversedText.text = userInput.reversed()
         }
     }
