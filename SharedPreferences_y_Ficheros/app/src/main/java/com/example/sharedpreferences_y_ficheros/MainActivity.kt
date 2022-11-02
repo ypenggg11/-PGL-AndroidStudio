05956b004deb7aaa8e005a3432d7d9dc57ce304e
xZ3NER Laptop <peng.asiacita@gmail.com>
2022-11-02 13:50:21 +0000

    Persistencia activity added

--- SharedPreferences_y_Ficheros/app/src/main/java/com/example/sharedpreferences_y_ficheros/MainActivity.kt
+++ SharedPreferences_y_Ficheros/app/src/main/java/com/example/sharedpreferences_y_ficheros/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.sharedpreferences_y_ficheros
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sharedpreferences_y_ficheros.databinding.ActivityMainBinding
 
 private lateinit var viewBinding: ActivityMainBinding
@@ -11,6 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        val preferences = getSharedPreferences("email_data", Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+
+        val grabarButton = viewBinding.grabarButton
+        val recuperarButton = viewBinding.recuperarButton
+
+        grabarButton.setOnClickListener {
+            val emailEditT = viewBinding.emailEditT
+            val datosEditT = viewBinding.datosEditT
+
+            if (emailEditT.text.isNotEmpty()&&datosEditT.text.isNotEmpty()){
+                editor.putString(emailEditT.text.toString(),datosEditT.text.toString())
+                editor.apply()
+
+                Toast.makeText(this,"Datos guardados",Toast.LENGTH_LONG).show()
+
+                emailEditT.setText("")
+                datosEditT.setText("")
+            }
+        }
 
 
     }
