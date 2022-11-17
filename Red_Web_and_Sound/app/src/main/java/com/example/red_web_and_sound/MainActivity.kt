03271d441cf1ff54bd2d6a609a3c73d9038aa18d
xZ3NER Laptop <peng.asiacita@gmail.com>
2022-11-17 11:12:43 +0000

    TODO Red_Web_and_Sound

--- Red_Web_and_Sound/app/src/main/java/com/example/red_web_and_sound/MainActivity.kt
+++ Red_Web_and_Sound/app/src/main/java/com/example/red_web_and_sound/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.red_web_and_sound
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.red_web_and_sound.databinding.ActivityMainBinding
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
-    //TODO Desde Apartado 7
+    //TODO desde el apartado 14.-
     private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +24,38 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding.checkConnButton.setOnClickListener {
             if (checkNet(this)) {
-                //val type = connType(this)
-                //showMessage("Conectado a Internet mediante ${type}")
+                val type = connType(this)
+                showMessage("Conectado a Internet mediante ${type}")
             }else{
-                //showMessage("No estas conectado a Internet")
+                showMessage("No estas conectado a Internet")
+            }
+        }
+
+        viewBinding.navButton.setOnClickListener {
+            val url = viewBinding.pageUrlEditT.text.toString()
+
+            if (url.isNotEmpty()) {
+                if (checkUrl(url)) {
+                    val webIntent = Intent(this@MainActivity,WebViewActivity::class.java)
+                    webIntent.putExtra("url", url)
+
+                    startActivity(webIntent)
+                    this.finish()
+                }else{
+                    showMessage("Formato de URL incorrecto")
+                }
+            }else{
+                showMessage("Inserte un url")
             }
         }
     }
 
-    fun checkNet(context: Context): Boolean {
+    private fun checkUrl(url:String): Boolean {
+
+        return Pattern.matches("^(https|http)://([a-zA-Z0-9](\\.)?(/)?)+",url)
+    }
+
+    private fun checkNet(context: Context): Boolean {
         val connectionManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
@@ -39,5 +66,28 @@ class MainActivity : AppCompatActivity() {
             val netCapabilities = connectionManager.getNetworkCapabilities(activeNetwork)
             return (netCapabilities!!.hasCapability(NET_CAPABILITY_INTERNET))
         }
+    }
+
+    private fun connType(context: Context): String {
+        val conectionManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            val connection = conectionManager.activeNetworkInfo
+            when(connection!!.type) {
+                ConnectivityManager.TYPE_MOBILE -> {return "Datos Móviles"}
+                ConnectivityManager.TYPE_WIFI -> {return "WIFI"}
+                else -> {return ""}
+            }
+        }else{
+            val activeNetwork = conectionManager.getNetworkCapabilities(conectionManager.activeNetwork)
+            if (activeNetwork!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) return  "Mobile data"
+            else
+                if (activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) return "Wifi"
+        }
+        return "Internet OFF"
+    }
+
+    private fun showMessage(message:String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 }
---
+++ Red_Web_and_Sound/app/src/main/java/com/example/red_web_and_sound/WebViewActivity.kt
@@ -0,0 +1,31 @@
+package com.example.red_web_and_sound
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import com.example.red_web_and_sound.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        val bundle = intent.extras
+        val webUrl:String? = bundle?.getString("url")
+
+        viewBinding.webView.webViewClient = WebViewClient()
+        viewBinding.webView.loadUrl(webUrl!!)
+
+        viewBinding.backButton.setOnClickListener {
+            val webIntent = Intent(this@WebViewActivity,MainActivity::class.java)
+            startActivity(webIntent)
+
+            this.finish()
+        }
+    }
+}
