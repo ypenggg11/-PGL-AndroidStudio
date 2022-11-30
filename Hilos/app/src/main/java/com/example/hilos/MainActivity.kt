2ad526bba0c98590aa2dd1115b7ff149c06144c9
xZ3NER Laptop <peng.asiacita@gmail.com>
2022-11-30 09:36:38 +0000

    TODO Hilos

---
+++ Hilos/app/src/main/java/com/example/hilos/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hilos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hilos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+
+    }
+}
